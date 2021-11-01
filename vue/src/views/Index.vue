8986e158742bab093aafe1286d2462f2d463c25e
Aman Mourya <aman@aman.mourya>
2021-11-01 15:12:51 +0530

    add initial

---
+++ vue/src/views/Index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div class="custom-header">
+		<img :src="image" />
+		<p>Crypstarter</p>
+		</div>
+		<div class="container">
+
+			<SpTokenSend :address="address" />
+			<SpTransferList :address="address" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Index',
+
+	   data() {
+   		return {
+   		image:require("/public/Mask Group.svg"),
+
+   		}
+   	},
+	computed: {
+		address() {
+			return this.$store.getters['common/wallet/address']
+		}
+	}
+}
+</script>
